d5ba976ff1f032a0ad6fd27023b29cac3ac7a420
kagchi <yonuxbot@gmail.com>
2024-07-31 14:25:01 +0700

    chore: initial hono setup

--- services/api/src/index.ts
+++ services/api/src/index.ts
@@ -1,1 +1,17 @@
-export const main = (): string => "Hello, world! ";
+import { serve } from "@hono/node-server";
+import { Hono } from "hono";
+import { pinoLogger } from "hono-pino-logger";
+import pino from "pino";
+const app = new Hono();
+
+const logger = pino({
+    transport: {
+        target: "pino-pretty"
+    }
+});
+
+app.use("*", pinoLogger(logger));
+
+app.get("/", c => c.text("Hello World"));
+
+serve({ fetch: app.fetch, port: 3001 });
