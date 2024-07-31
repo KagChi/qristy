a344f4c2cde9a7ac90ef12d4e57f96f11f44b959
kagchi <yonuxbot@gmail.com>
2024-07-31 22:34:10 +0700

    feat: setup socket.io

--- services/api/src/index.ts
+++ services/api/src/index.ts
@@ -2,6 +2,7 @@ import { serve } from "@hono/node-server";
 import { Hono } from "hono";
 import { pinoLogger } from "hono-pino-logger";
 import pino from "pino";
+import { Server } from "socket.io";
 
 const app = new Hono();
 const logger = pino({
@@ -14,6 +15,11 @@ app.use("*", pinoLogger(logger));
 
 app.get("/", c => c.text("Hello World"));
 
-serve({ fetch: app.fetch, port: 3001 }, info => {
+const server = serve({ fetch: app.fetch, port: 3001 }, info => {
     logger.info(`Server is running on port ${info.port}`);
+});
+
+const io = new Server(server);
+io.on("connection", socket => {
+    logger.info(`Client connected: ${socket.id}`);
 });
