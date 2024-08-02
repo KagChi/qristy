2098f315653e8b38aa5394404f21652325f846e4
kagchi <yonuxbot@gmail.com>
2024-08-02 15:12:13 +0700

    fix: specify api url on env

--- services/website/src/actions/transactions.ts
+++ services/website/src/actions/transactions.ts
@@ -1,3 +1,10 @@
+/* eslint-disable @typescript-eslint/naming-convention */
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
+const config = {
+    // @ts-expect-error Typings error, idk to fix it.
+    API_URL: import.meta.env.VITE_API_URL ?? "http://localhost:3001"
+};
+
 export interface Transaction {
     id: string;
     createdAt: string;
@@ -13,7 +20,7 @@ export interface Transaction {
 
 export const fetchTransactions = async (page: number): Promise<Transaction[]> => {
     try {
-        const response: Response = await fetch(`/api/transactions?page=${page}`);
+        const response: Response = await fetch(`${config.API_URL}/transactions?page=${page}`);
         if (!response.ok) {
             throw new Error("Failed to fetch transactions");
         }
@@ -27,7 +34,7 @@ export const fetchTransactions = async (page: number): Promise<Transaction[]> =>
 
 export const fetchTransaction = async (transactionId: string): Promise<Transaction> => {
     try {
-        const response: Response = await fetch(`/api/transaction/${transactionId}`);
+        const response: Response = await fetch(`${config.API_URL}/transaction/${transactionId}`);
         if (!response.ok) {
             throw new Error("Failed to fetch transactions");
         }
@@ -40,7 +47,7 @@ export const fetchTransaction = async (transactionId: string): Promise<Transacti
 
 export const createTransaction = async (amount: number, withTax: boolean): Promise<Transaction> => {
     try {
-        const response: Response = await fetch("/api/create", {
+        const response: Response = await fetch(`${config.API_URL}/create`, {
             method: "POST",
             body: JSON.stringify({
                 amount,
